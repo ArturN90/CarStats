9c1cfd51742e0f696b851de4766a819378c23cac
ArturN90 <nowak.art90@gmail.com>
2022-10-20 11:48:42 +0200

    small up

--- src/CarStats/scraping/scraper.py
+++ src/CarStats/scraping/scraper.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import re
-
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
+
 from CarStats.log import log
 
 
