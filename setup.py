502e74ea242cbdb3c21288546a423102889eb544
ArturN90 <nowak.art90@gmail.com>
2022-10-27 08:30:48 +0200

    improvements

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python
-
+"""
+Instalation setup of CarStats
+"""
 import os
 from setuptools import setup, find_packages
 
+
 def read(fname):
+    """set up path"""
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     reqs = f.read().splitlines()
 
 setup(
-    name = "CarStats",
-    version = "0.0.1",
-    author = "Artur Nowak",
-    keywords = "Carstats, car statistic",
-    long_description=read('README.md'),
-    package_dir={"": "src"},                                                    
-    packages=find_packages("src"), 
+    name="CarStats",
+    version="0.0.1",
+    author="Artur Nowak",
+    keywords="Carstats, car statistic",
+    long_description=read("README.md"),
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     install_requires=reqs,
-    python_requires='>=3.8, <3.9',
+    python_requires=">=3.8, <3.9",
     classifiers=[
         "Development Status :: 1 - Pre-alpha",
         "Environment :: Console",
